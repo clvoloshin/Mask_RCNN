9859a888baa3ae3f3180001a28f7048e19804fa7
Cameron Voloshin <clvoloshin@Camerons-MacBook-Pro.local>
2019-12-04 13:49:29 -0800

    mhp

---
+++ samples/MHP/mhp.py
@@ -0,0 +1,264 @@
+"""
+Mask R-CNN
+Train on the toy MHP dataset and implement color splash effect.
+
+Copyright (c) 2018 Matterport, Inc.
+Licensed under the MIT License (see LICENSE for details)
+Written by Waleed Abdulla
+
+------------------------------------------------------------
+
+Usage: import the module (see Jupyter notebooks for examples), or run from
+       the command line as such:
+
+    # Train a new model starting from pre-trained COCO weights
+    python3 mhp.py train --dataset=/path/to/mhp/dataset --weights=coco
+    python mhp.py train --dataset ../../datasets/MHP/ --weights coco --logs ./logs
+
+    # Resume training a model that you had trained earlier
+    python3 mhp.py train --dataset=/path/to/mhp/dataset --weights=last
+
+    # Train a new model starting from ImageNet weights
+    python3 mhp.py train --dataset=/path/to/mhp/dataset --weights=imagenet
+"""
+
+import os
+import sys
+import json
+import datetime
+import numpy as np
+import skimage.draw
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import model as modellib, utils
+
+# Path to trained weights file
+COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+
+# Directory to save logs and model checkpoints, if not provided
+# through the command line argument --logs
+DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+############################################################
+#  Configurations
+############################################################
+
+
+class MHPConfig(Config):
+    """Configuration for training on the toy  dataset.
+    Derives from the base Config class and overrides some values.
+    """
+    # Give the configuration a recognizable name
+    NAME = "mhp"
+
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 2
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # Background + person
+
+    # Number of training steps per epoch
+    STEPS_PER_EPOCH = 100
+
+    # Skip detections with < 90% confidence
+    DETECTION_MIN_CONFIDENCE = 0.9
+
+
+############################################################
+#  Dataset
+############################################################
+
+class MHPDataset(utils.Dataset):
+
+    def load_mhp(self, dataset_dir, subset):
+        """Load a subset of the MHP dataset.
+        dataset_dir: Root directory of the dataset.
+        subset: Subset to load: train or val
+        """
+        # Add classes. We have only one class to add.
+        self.add_class("mhp", 1, "person")
+
+        # Train or validation dataset?
+        assert subset in ["train", "val"]
+        dataset_dir = os.path.join(dataset_dir, subset)
+
+        # Preprocess mask dir
+        image_ids = next(os.walk(os.path.join(dataset_dir, 'parsing_annos')))[2]
+        self._masks_ims = {}
+        for image_id in image_ids:
+            num = image_id.split('_')[0]
+            if num in self._masks_ims:
+                self._masks_ims[num].append(image_id)
+            else:
+                self._masks_ims[num] = [image_id]
+
+        # Get image ids from file names
+        image_ids = next(os.walk(os.path.join(dataset_dir, 'images')))[2]
+
+        # Add images
+        for image_id in image_ids:
+            self.add_image(
+                "mhp",
+                image_id=image_id,
+                path=os.path.join(dataset_dir, 'images', image_id))
+
+    def load_mask(self, image_id):
+        """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+
+        info = self.image_info[image_id]
+
+        # Get mask directory from image path
+        num = info['id'].split('.jpg')[0]
+
+
+        masks_paths = [os.path.join(os.path.dirname(os.path.dirname(info['path'])), "parsing_annos", mask) for mask in self._masks_ims[num]]
+
+        # Read mask files from .png image
+        mask = []
+        for f in masks_paths:
+            if f.endswith(".png"):
+                m = skimage.io.imread(f)
+                m = m[...,:-1].sum(axis=-1) > 0 # find nonzero
+                mask.append(m)
+        mask = np.stack(mask, axis=-1)
+        # Return mask, and array of class IDs of each instance. Since we have
+        # one class ID, we return an array of ones
+        return mask, np.ones([mask.shape[-1]], dtype=np.int32)
+
+    def image_reference(self, image_id):
+        """Return the path of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "mhp":
+            return info["path"]
+        else:
+            super(self.__class__, self).image_reference(image_id)
+
+
+def train(model):
+    """Train the model."""
+    # Training dataset.
+    dataset_train = MHPDataset()
+    dataset_train.load_mhp(args.dataset, "train")
+    dataset_train.prepare()
+
+    # Validation dataset
+    dataset_val = MHPDataset()
+    dataset_val.load_mhp(args.dataset, "val")
+    dataset_val.prepare()
+
+    # Relearning the head of the network
+    print("Training network heads")
+    model.train(dataset_train, dataset_val,
+                learning_rate=config.LEARNING_RATE,
+                epochs=100,
+                layers='heads')
+
+
+############################################################
+#  Training
+############################################################
+
+if __name__ == '__main__':
+    import argparse
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Train Mask R-CNN to detect persons.')
+    parser.add_argument("command",
+                        metavar="<command>",
+                        help="'train' or 'splash'")
+    parser.add_argument('--dataset', required=False,
+                        metavar="/path/to/mhp/dataset/",
+                        help='Directory of the MHP dataset')
+    parser.add_argument('--weights', required=True,
+                        metavar="/path/to/weights.h5",
+                        help="Path to weights .h5 file or 'coco'")
+    parser.add_argument('--logs', required=False,
+                        default=DEFAULT_LOGS_DIR,
+                        metavar="/path/to/logs/",
+                        help='Logs and checkpoints directory (default=logs/)')
+    # parser.add_argument('--image', required=False,
+    #                     metavar="path or URL to image",
+    #                     help='Image to apply the color splash effect on')
+    # parser.add_argument('--video', required=False,
+    #                     metavar="path or URL to video",
+    #                     help='Video to apply the color splash effect on')
+    args = parser.parse_args()
+
+    # Validate arguments
+    if args.command == "train":
+        assert args.dataset, "Argument --dataset is required for training"
+    # elif args.command == "splash":
+    #     assert args.image or args.video,\
+    #            "Provide --image or --video to apply color splash"
+
+    print("Weights: ", args.weights)
+    print("Dataset: ", args.dataset)
+    print("Logs: ", args.logs)
+
+    # Configurations
+    if args.command == "train":
+        config = MHPConfig()
+    else:
+        class InferenceConfig(MHPConfig):
+            # Set batch size to 1 since we'll be running inference on
+            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+            GPU_COUNT = 1
+            IMAGES_PER_GPU = 1
+        config = InferenceConfig()
+    config.display()
+
+    # Create model
+    if args.command == "train":
+        model = modellib.MaskRCNN(mode="training", config=config,
+                                  model_dir=args.logs)
+    else:
+        model = modellib.MaskRCNN(mode="inference", config=config,
+                                  model_dir=args.logs)
+
+    # Select weights file to load
+    if args.weights.lower() == "coco":
+        weights_path = COCO_WEIGHTS_PATH
+        # Download weights file
+        if not os.path.exists(weights_path):
+            utils.download_trained_weights(weights_path)
+    elif args.weights.lower() == "last":
+        # Find last trained weights
+        weights_path = model.find_last()
+    elif args.weights.lower() == "imagenet":
+        # Start from ImageNet trained weights
+        weights_path = model.get_imagenet_weights()
+    else:
+        weights_path = args.weights
+
+    # Load weights
+    print("Loading weights ", weights_path)
+    if args.weights.lower() == "coco":
+        # Exclude the last layers because they require a matching
+        # number of classes
+        model.load_weights(weights_path, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask"])
+    else:
+        model.load_weights(weights_path, by_name=True)
+
+    # Train or evaluate
+    if args.command == "train":
+        train(model)
+    elif args.command == "splash":
+        detect_and_color_splash(model, image_path=args.image,
+                                video_path=args.video)
+    else:
+        print("'{}' is not recognized. "
+              "Use 'train' or 'splash'".format(args.command))
